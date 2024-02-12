7abd0674f0689dca51d993cd7d8af8ccbd77a70e
Eunseo Seol <147463468+eunseoseol@users.noreply.github.com>
2024-02-12 18:08:45 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,219 +1,71 @@
 "use client";
-
-
-import React, { useEffect, useState } from 'react';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, doc, onSnapshot } from 'firebase/firestore';
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from 'firebase/auth';
-import { loadTossPayments } from "@tosspayments/payment-sdk";
-
-// Firebase 설정 정보
-const firebaseConfig = {
-  apiKey: "AIzaSyBlLWD-ifPY8So3R2JIQEB_PjRiTUKn7no",
-  authDomain: "eunseo-seol-6334e.firebaseapp.com",
-  projectId: "eunseo-seol-6334e",
-  storageBucket: "eunseo-seol-6334e.appspot.com",
-  messagingSenderId: "1042386782911",
-  appId: "1:1042386782911:web:7c833a17dc9f505dd3140a",
-  measurementId: "G-5RN0P4M3JE"
-};
-
-// Firebase 앱 초기화
-initializeApp(firebaseConfig);
-const auth = getAuth();
-const firestore = getFirestore();
-const imageStyle = {
-  maxWidth: '150px',
-  maxHeight: '150px', // 정사각형 비율로 조정
-  marginBottom: '20px',
-  borderRadius: '10px', // 모서리 둥글게 처리
-  transition: 'transform 0.3s ease', // 부드러운 변환 효과
-  marginTop: '20px', // 상단에 패딩 추가
-
-};
+import React, { useState, useEffect } from "react";
 
 export default function Page() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isCitizenshipTrue, setIsCitizenshipTrue] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [storeUrl, setStoreUrl] = useState("");
+
+  const hoverStyle = {
+    maxWidth: '150px',
+    marginBottom: '20px',
+  };
+  const mobileHoverStyle = {
+    maxWidth: '80px',
+    marginBottom: '10px',
+  };
 
   useEffect(() => {
-    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setIsLoggedIn(true);
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isAppleDevice = /iphone|ipad|ipod|macintosh/.test(userAgent);
 
-        // Firestore에서 사용자의 문서 감시
-        const userDocRef = doc(firestore, "users", user.email);
-        const unsubscribeDoc = onSnapshot(userDocRef, (doc) => {
-          const userData = doc.data();
-          if (userData && userData.Citizenship === true) {
-            setIsCitizenshipTrue(true);
-          } else {
-            setIsCitizenshipTrue(false);
-          }
-        });
+    if (isAppleDevice) {
+      setStoreUrl("https://apps.apple.com/us/app/edith-space-assistant/id6450296741");
+    } else {
+      setStoreUrl("https://play.google.com/store/apps/details?id=com.openai.chatgpt&hl=en_US");
+    }
 
-        return unsubscribeDoc;
-      } else {
-        setIsLoggedIn(false);
-        setIsCitizenshipTrue(false);
-      }
-    });
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Consider below 768px as mobile
+    };
 
-    return unsubscribeAuth;
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const handleGoogleLogin = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      const result = await signInWithPopup(auth, provider);
-      // 로그인 성공 처리
-      setIsLoggedIn(true);
-      console.log('Logged in user:', result.user);
-    } catch (error) {
-      // 오류 처리
-      console.error('Login failed:', error);
-    }
-  };
-
-  const handleLogout = () => {
-    signOut(auth).then(() => {
-      // 로그아웃 성공 처리
-      setIsLoggedIn(false);
-    }).catch((error) => {
-      // 오류 처리
-      console.error('Logout failed:', error);
-    });
-  };
-
-  const handleClick = async () => {
-    const tossPayments = await loadTossPayments(
-      "test_ck_QbgMGZzorznJWP7nelA2rl5E1em4"
-    );
-
-    await tossPayments.requestPayment("카드", {
-      amount: 100000,
-      orderId: Math.random().toString(36).slice(2),
-      orderName: "스타크 인더스트리즈 티셔츠",
-      successUrl: `${window.location.origin}/api/payments`,
-      failUrl: `${window.location.origin}/api/payments/fail`,
-    });
-  };
-  const handleClickhood = async () => {
-    const tossPayments = await loadTossPayments(
-      "test_ck_QbgMGZzorznJWP7nelA2rl5E1em4"
-    );
-
-    await tossPayments.requestPayment("카드", {
-      amount: 100000,
-      orderId: Math.random().toString(36).slice(2),
-      orderName: "스타크 인더스트리즈 후드집업",
-      successUrl: `${window.location.origin}/api/payments`,
-      failUrl: `${window.location.origin}/api/payments/fail`,
-    });
-  };
-  const handleClicktumblr = async () => {
-    const tossPayments = await loadTossPayments(
-      "test_ck_QbgMGZzorznJWP7nelA2rl5E1em4"
-    );
-
-    await tossPayments.requestPayment("카드", {
-      amount: 100000,
-      orderId: Math.random().toString(36).slice(2),
-      orderName: "스타크 인더스트리즈 텀블러",
-      successUrl: `${window.location.origin}/api/payments`,
-      failUrl: `${window.location.origin}/api/payments/fail`,
-    });
-  };
+  const imageStyle = isMobile ? mobileHoverStyle : hoverStyle;
 
   return (
-    <div style={{ position: 'relative', width: '100vw', height: '100vh',overflowY: 'scroll' }}>
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
       <video autoPlay loop muted playsInline style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', objectFit: 'cover', zIndex: -1 }}>
         <source src="/background.mp4" type="video/mp4" />
       </video>
-      {isLoggedIn ? (
-        <>
-          {isCitizenshipTrue ? (
-            <div style={{ position: 'relative', zIndex: 1, width: '100%', paddingTop: '20px', height: 'calc(100vh - 20px)', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <img src="/book.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-            <h1 style={{ fontSize: '16px', fontWeight: 'bold', color: 'white' }}>
-             Stark Industries
-            </h1>
-            <h1 style={{ fontSize: '16px', fontWeight: 'bold', color: 'white' }}>
-            Chaging the world for a better future.
-            </h1>
 
-         <footer style={{ textAlign: 'center', marginTop: '0px', padding: '10px', fontSize: '14px', color: 'white' }}>
-         <button onClick={handleLogout}>Sign Out</button>
-
+      <div style={{ position: 'relative', zIndex: 1, width: '100%', paddingTop: '20px', paddingBottom: '100px', height: 'calc(100vh - 20px)', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <main className="p-4" style={{ height: '100vh' }}>
+          <div style={{ position: 'relative', zIndex: 1, paddingBottom: '40px', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '-50px' }}>
+            <a href={storeUrl}>
+              <img src="/book.png" alt="Book" style={imageStyle} />
+            </a>
+            <h1 style={{ fontSize: isMobile ? '16px' : '48px', fontWeight: 'bold', color: 'white' }}>
+            Stark Industries
+            </h1>
+            <h1 style={{ fontSize: isMobile ? '16px' : '48px', fontWeight: 'bold', color: 'white' }}>
+            Changing the world for a better future.
+            </h1>
+              <footer style={{ textAlign: 'center', marginTop: '0px', padding: '10px', fontSize: '14px', color: 'white' }}>
 <p>Copyright ⓒ Stark Industries. All Rights Reserved</p>
-<p>248-27-01947 | Eunseo Seol | +82 010-3839-7911 | 39-26, Pungyeong-ro 170beon-gil, Gwangsan-gu, Gwangju, Republic of Korea</p>
+<p>Eunseo Seol | 39–26, Pungyeong-ro 170beon-gil, Gwangsan-gu, Gwangju, Republic of Korea | 248–27–01947 </p>
 </footer>
+          </div>
 
-       </div>
 
-          ) : (
-            
-            <div style={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '100vh'
-            }}>
-              <img src="/t-shirts.png" alt="Stark Industries T-Shirt" 
-       style={imageStyle}
-/>
-                <h1 style={{ fontSize: '16px', fontWeight: 'bold', color: 'white' }}>
-            스타크 인더스트리즈 티셔츠
-           </h1>
-         
-            <button onClick={handleClick}>구매하기</button>
-            <img src="/tumblr.png" alt="Stark Industries T-Shirt" 
-       style={imageStyle}
-/>
-                <h1 style={{ fontSize: '16px', fontWeight: 'bold', color: 'white' }}>
-            스타크 인더스트리즈 텀블러
-           </h1>
-         
-            <button onClick={handleClicktumblr}>구매하기</button>
+              
+        </main>
+      </div>
 
-            <img src="/zipup.png" alt="Stark Industries T-Shirt" 
-       style={imageStyle}
-/>                <h1 style={{ fontSize: '16px', fontWeight: 'bold', color: 'white' }}>
-            스타크 인더스트리즈 후드집업
-           </h1>
-         
-            <button onClick={handleClickhood}>구매하기</button>
-            </div>
 
-          )}
-        </>
-      ) : (
-
-<div style={{
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100vh'
-}}>
-  <img src="/book.png" alt="Book" style={{ maxWidth: '150px', marginBottom: '20px' }} />
-  <h1 style={{ fontSize: '16px', fontWeight: 'bold', color: 'white' }}>
-             Stark Industries
-            </h1>
-            <h1 style={{ fontSize: '16px', fontWeight: 'bold', color: 'white' }}>
-            Chaging the world for a better future.
-            </h1>
-  <button onClick={handleGoogleLogin}>Sign In</button>
-  <footer style={{ textAlign: 'center', marginTop: '0px', padding: '10px', fontSize: '14px', color: 'white' }}>
-
-<p>Copyright ⓒ Stark Industries. All Rights Reserved</p>
-<p>248-27-01947 | Eunseo Seol | +82 010-3839-7911 | 39-26, Pungyeong-ro 170beon-gil, Gwangsan-gu, Gwangju, Republic of Korea</p>
-</footer>
-</div>
-
-      )}
-          
+              
     </div>
   );
 }
